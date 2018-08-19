b83c8bffd9b6a0746843d9915ee4f3389d763367
RomanCheprack <romancheprack@gmail.com>
2018-08-19 17:34:32 +0300

    midified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'open-uri'
+
+User.destroy_all
+Kid.destroy_all
+Ebook.destroy_all
+Prize.destroy_all
+
+puts "creating user"
+user = User.new(
+  username: 'readcoin',
+  email: '1234@1234.com',
+  password: '1234'
+  )
+
+puts "creating kids"
+kid1 = Kid.new(
+  name: 'kid',
+  password: '1',
+  age: '8',
+  intreset: 'sci-fi',
+  wallet: '150',
+  avatar: Faker::Avatar.image("my-own-slug", "50x50", "jpg")
+  )
+
+kid2 = Kid.new(
+  name: 'kid2',
+  password: '2',
+  age: Faker::Number.non_zero_digit,
+  interest: 'fashion',
+  wallet: '100',
+  avatar: Faker::Avatar.image("my-own-slug", "50x50", "jpg")
+  )
+
+puts "creating ebooks.."
+
+5.times do
+  ebook = Ebook.new(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    summery: 'it was a wonder day as i walk in the forest...'
+    url: 'http://www.planetpublish.com/wp-content/uploads/2011/11/The_Jungle_Book_T.pdf',
+    quiz:
+    readcoin: Faker::Number.between(1, 15),
+    genre: Faker::Book.genre
+    )
+end
+
+puts "creating prizes.."
+
+5.times do
+  prize = Prize.new(
+  price: Faker::Number.non_zero_digit,
+  title: Faker::Commerce.product_name,
+  picture: Faker::LoremPixel.image("60x60", true)
+ )
+
+end
+
+user.create!
+kid1.create!
+kid2.create!
+ebook.create!
+prize.create!
+
+puts "finished seeding"
