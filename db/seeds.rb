5903d2f5d61cc32bd14e3de5caffda389167efd5
RomanCheprack <romancheprack@gmail.com>
2018-08-19 15:47:31 +0300

    adding some seeds and faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Kid.destroy_all
+Ebook.destroy_all
+Prize.destroy_all
+
+puts "creating user"
+user = User.create!(
+  username: 'readcoin',
+  email: '1234@1234.com',
+  password: '1234'
+  )
+
+puts "creating kids"
+kid = Kid.create!(
+  name: 'kid',
+  password: '1',
+  age: '8',
+  intreset: 'sci-fi',
+  wallet: '150',
+  avatar: url)
+
+kid = Kid.create!(
+  name: 'kid2',
+  password: '2'
+  age: Faker::Number.non_zero_digit
+  interest:
+  wallet:
+  avatar:
+  )
+
+puts "creating ebooks.."
+
+5.times do
+  ebook = Ebook.create!(
+    title:
+    url:
+    quiz:
+    readcoin: Faker::Number.between(1, 10)
+    )
+end
+
+puts "creating prizes.."
+
+5.times do
+  prize = Prize.create!(
+  price: Faker::Number.non_zero_digit,
+  title:
+  picture:
+ )
+
+end
