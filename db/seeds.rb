b889384927b1355b350a8ba17c9f4b95965c9af9
RomanCheprack <romancheprack@gmail.com>
2018-08-19 16:27:32 +0300

    adding more to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,48 +6,58 @@ Ebook.destroy_all
 Prize.destroy_all
 
 puts "creating user"
-user = User.create!(
+user = User.new(
   username: 'readcoin',
   email: '1234@1234.com',
   password: '1234'
   )
 
 puts "creating kids"
-kid = Kid.create!(
+kid1 = Kid.new(
   name: 'kid',
   password: '1',
   age: '8',
   intreset: 'sci-fi',
   wallet: '150',
-  avatar: url)
+  avatar: Faker::Avatar.image("my-own-slug", "50x50", "jpg")
+  )
 
-kid = Kid.create!(
+kid2 = Kid.new(
   name: 'kid2',
-  password: '2'
-  age: Faker::Number.non_zero_digit
-  interest:
-  wallet:
-  avatar:
+  password: '2',
+  age: Faker::Number.non_zero_digit,
+  interest: 'fashion',
+  wallet: '100',
+  avatar: Faker::Avatar.image("my-own-slug", "50x50", "jpg")
   )
 
 puts "creating ebooks.."
 
 5.times do
-  ebook = Ebook.create!(
-    title:
+  ebook = Ebook.new(
+    title: Faker::Book.title,
     url:
     quiz:
-    readcoin: Faker::Number.between(1, 10)
+    readcoin: Faker::Number.between(1, 15),
+    genre: Faker::Book.genre
     )
 end
 
 puts "creating prizes.."
 
 5.times do
-  prize = Prize.create!(
+  prize = Prize.new(
   price: Faker::Number.non_zero_digit,
   title:
-  picture:
+  picture: Faker::LoremPixel.image("60x60", true)
  )
 
 end
+
+user.create!
+kid1.create!
+kid2.create!
+ebook.create!
+prize.create!
+
+puts "finished seeding"
