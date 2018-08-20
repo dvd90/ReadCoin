372de492c58c672c1af6ff63f86671e4cc60e1b1
Lorene Valency <lorene.herzog@gmail.com>
2018-08-20 15:04:29 +0300

    dashboard for kids

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   end
   resources :prizes, only: [:index, :show]
   get 'login', to: 'kids#login', as: :kids_login
+  post 'login', to: 'kids#create_login', as: :verify_password
+  get 'kid_dashboard/:id', to: 'kids#kid_dashboard', as: :kid_dashboard
 end
