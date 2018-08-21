82a758edeedf50a206cd3614d17123bcaf0df417
David Sellam <dvdsellam@gmail.com>
2018-08-21 14:30:31 +0300

    purchases

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,21 @@
+class PurchasesController < ApplicationController
+
+  def show
+    @purchase = Purchase.find(params[:id])
+  end
+
+  def create
+    @purchase = Purchase.new
+    @kid = Kid.find(params[:kid_id])
+    @prize = Prize.find(params[:prize_id])
+    @kid.wallet -= @prize.price
+    @kid.save!
+    @purchase.kid = @kid
+    @purchase.prize = @prize
+    if @purchase.save!
+      redirect_to kid_prize_purchase_path(@kid, @prize, @purchase)
+    else
+      redirect_to kid_prizes_path(@kid, @prize)
+    end
+  end
+end
