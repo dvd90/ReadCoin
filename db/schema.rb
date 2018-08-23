e25dbc237aeafe8c5aa69f61e04775b8d771dafb
Lorene Valency <lorene.herzog@gmail.com>
2018-08-23 16:29:49 +0300

    removed admins

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2018_08_23_091217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "url"
