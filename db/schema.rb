f54ce41285938d549b263a863bb92d5439c626a6
David Sellam <dvdsellam@gmail.com>
2018-08-23 13:32:28 +0300

    crmbooks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_155745) do
+ActiveRecord::Schema.define(version: 2018_08_23_091217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_155745) do
     t.integer "max_age"
     t.string "author"
     t.text "summary"
+    t.string "photo"
   end
 
   create_table "kids", force: :cascade do |t|
