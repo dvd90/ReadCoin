d99e3dd0b47d4aa47cba16253e4261eb48443708
Lorene Valency <lorene.herzog@gmail.com>
2018-08-26 18:39:33 +0300

    admns

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 2018_08_26_145209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
