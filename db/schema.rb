974ee9b7fe3bb29bb6e0f9bf893a6b98010fc78a
Lorene Valency <lorene.herzog@gmail.com>
2018-08-26 16:32:23 +0300

    some css on the login page

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 2018_08_26_122559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -89,6 +94,7 @@ ActiveRecord::Schema.define(version: 2018_08_26_122559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "username"
     t.integer "wallet_cents", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
